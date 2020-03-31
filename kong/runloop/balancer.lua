583c7a89ac20f1d39b7dbc323eb660cd05e5985b
Vinicius Mignot <vinicius.mignot@gmail.com>
2020-03-31 19:37:13 -0300

    perf(balancer) load upstreams before cleaning cache
    When there are a large number of upstreams in the database or the
    database is under heavy load, loading upstreams will take longer and,
    sometimes, unacceptably longer. This change address said issue by
    keeping the stale cache until the new data is loaded from the database.
    As there might be stale content with this change, this behavior only
    exists when worker consistency is set to eventual.

--- kong/runloop/balancer.lua
+++ kong/runloop/balancer.lua
@@ -839,9 +839,10 @@ local function update_balancer_state(premature)
 
   concurrency.with_coroutine_mutex(opts, function()
     if is_worker_state_stale() then
+      local updated_upstreams_dict = load_upstreams_dict_into_memory()
       singletons.core_cache:invalidate_local("balancer:upstreams")
       local _, err = singletons.core_cache:get("balancer:upstreams",
-                      { neg_ttl = 10 }, load_upstreams_dict_into_memory)
+                      { neg_ttl = 10 }, function() return updated_upstreams_dict end)
       if err then
         log(CRIT, "failed updating list of upstreams: ", err)
       else
